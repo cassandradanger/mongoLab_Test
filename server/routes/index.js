5c17b09ee9800dd5cf50fcd4e18d7eda1d25282e
Casie Lynch <cds.lynch@gmail.com>
2015-08-18 14:54:35 -0500

    Turned mongoose back on

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,26 +3,26 @@ var router = express.Router();
 var mongoose = require("mongoose");
 var path = require("path");
 
-//mongoose.connect("mongodb://casie:kubRA6Ub@ds035683.mongolab.com:35683/mongolab_test");
-//
-//var Cat = mongoose.model("cat", {name:String});
-//
-//router.post('/add', function(request, response, next){
-//    var kitty = new Cat({name: request.body.name});
-//    kitty.save(function(err){
-//        if(err) console.log("meows", err);
-//        response.send(kitty.toJSON());
-//        next();
-//    });
-//});
-//
-//router.get('/cats', function(request, response, next){
-//    return Cat.find({}).exec(function(err, cats){
-//        if(err) throw new Error(err);
-//        response.send(JSON.stringify(cats));
-//        next()
-//    });
-//});
+mongoose.connect("mongodb://casie:kubRA6Ub@ds035683.mongolab.com:35683/mongolab_test");
+
+var Cat = mongoose.model("cat", {name:String});
+
+router.post('/add', function(request, response, next){
+    var kitty = new Cat({name: request.body.name});
+    kitty.save(function(err){
+        if(err) console.log("meows", err);
+        response.send(kitty.toJSON());
+        next();
+    });
+});
+
+router.get('/cats', function(request, response, next){
+    return Cat.find({}).exec(function(err, cats){
+        if(err) throw new Error(err);
+        response.send(JSON.stringify(cats));
+        next()
+    });
+});
 
 router.get("/*", function(req, res, next) {
     console.log("Here is a console log");
