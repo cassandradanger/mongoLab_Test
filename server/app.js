d32e2268b36ee9876a118c77e515447f9c2925e0
Casie Lynch <cds.lynch@gmail.com>
2015-08-18 15:39:24 -0500

    double post fixing

--- server/app.js
+++ server/app.js
@@ -7,8 +7,6 @@ var index = require('./routes/index');
 app.use(bodyParser.json());
 app.use("/", index);
 
-app.use('/', index);
-
 app.set("port", (process.env.PORT || 5000));
 
 app.listen(app.get("port"), function(){
@@ -16,3 +14,19 @@ app.listen(app.get("port"), function(){
 });
 
 module.exports = app;
+
+
+//var express = require("express");
+//var app = express();
+//var index  = require("./routes/index");
+//var bodyParser = require('body-parser');
+//
+//var server = app.listen(3000, function() {
+//    var port = server.address().port;
+//    console.log("Listening on port: ", port);
+//});
+//
+//app.use(bodyParser.json());
+//app.use("/", index);
+//
+//module.exports = app;
