cceb43d490008d5ed8c5a2fcf5fcea24a66c5885
Casie Lynch <cds.lynch@gmail.com>
2015-08-18 14:52:31 -0500

    another test

--- server/app.js
+++ server/app.js
@@ -1,16 +1,31 @@
-var express = require("express");
+//var express = require("express");
+//var app = express();
+//var index  = require("./routes/index");
+//var bodyParser = require('body-parser');
+//
+//var server = app.set("port", (process.env.PORT || 5000));
+//
+//app.listen(app.get("port"), function() {
+//    var port = server.address().port;
+//    console.log("Listening on port: ", port);
+//});
+//
+//app.use(bodyParser.json());
+//app.use("/", index);
+//
+//module.exports = app;
+
+var express = require('express');
 var app = express();
-var index  = require("./routes/index");
-var bodyParser = require('body-parser');
 
-var server = app.set("port", (process.env.PORT || 5000));
+var index = require('./routes/index');
 
-app.listen(app.get("port"), function() {
-    var port = server.address().port;
-    console.log("Listening on port: ", port);
+app.use('/', index);
+
+app.set("port", (process.env.PORT || 5000));
+
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: " + app.get("port"));
 });
-
-app.use(bodyParser.json());
-app.use("/", index);
 
 module.exports = app;
