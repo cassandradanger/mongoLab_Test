978683424d41187138d170a0e7784d395f50eec8
Casie Lynch <cds.lynch@gmail.com>
2015-08-18 14:34:57 -0500

    db update

--- server/app.js
+++ server/app.js
@@ -3,13 +3,13 @@ var app = express();
 var index  = require("./routes/index");
 var bodyParser = require('body-parser');
 
-var server = app.listen(3000, function() {
-    var port = server.address().port;
-    console.log("Listening on port: ", port);
-});
+var server = app.set("port", (process.env.PORT || 5000));
+    //app.listen(3000, function() {
+    //var port = server.address().port;
+//    console.log("Listening on port: ", port);
+//});
 
 app.use(bodyParser.json());
 app.use("/", index);
 
 module.exports = app;
-
